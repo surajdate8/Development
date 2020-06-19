2e40bba3897173f452c90269a18f04ab8c3e471e
surajdate8 <surajdate8@gmail.com>
2020-06-19 18:49:07 +0530

    Spring Boot Tutorials | Full Course Till : 41:00

--- SpringBootFirstProject/src/main/java/com/example/demo/SpringBootFirstProjectApplication.java
+++ SpringBootFirstProject/src/main/java/com/example/demo/SpringBootFirstProjectApplication.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstProjectApplication.class, args);
+		ConfigurableApplicationContext context= SpringApplication.run(SpringBootFirstProjectApplication.class, args);
 		System.out.println("Hi");
+		
+		Alien a = context.getBean(Alien.class);
+		a.show();
 	}
 
 }
