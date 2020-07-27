d7da68e4c21f11a1094959e596f725ffe45a4df3
surajdate8 <surajdate8@gmail.com>
2020-07-27 22:24:10 +0530

    Initial commit

---
+++ Data_Structure/src/main/java/com/primitive/type/Array.java
@@ -0,0 +1,18 @@
+package com.primitive.type;
+
+public class Array {
+	public static void main(String[] args) {
+		System.out.println("Data Structure started...");
+				
+		int[] arr=new int[10];
+		
+		for(int i=0;i<10;i++) {
+			arr[i]=i;
+		}
+		for(int i=0;i<10;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
