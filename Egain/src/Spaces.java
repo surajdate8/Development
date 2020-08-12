77e8e2ebb1eb47f742d9f6591f7a022375038d49
surajdate8 <surajdate8@gmail.com>
2020-08-12 22:10:53 +0530

    Second Program

--- Egain/src/Spaces.java
+++ Egain/src/Spaces.java
@@ -5,34 +5,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Spaces {
-	static int white=0;
-	static String stream_set=null;
+	static String stream_set="Hey , r u free              now?";
 	static String path=null;
 
 	public static void main(String[] args) throws FileNotFoundException{
-
-		input();
 		int count=process(stream_set);
-		output(count);		
 	}
-	private static void input() throws FileNotFoundException {
-		path=System.getProperty("user.dir");
-		String filename = path+"/InputOutput/input.txt";
-		BufferedReader reader = new BufferedReader(new FileReader(filename));
-
-		try{
-			String line;
-
-			while((line = reader.readLine()) != null){ 
-				stream_set=line;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	
 	private static int process(String stream_set) {
 		int top=-1,length=0,white=0;
-
 		for(int i=0;i<stream_set.length();i++)
 		{
 			char c=stream_set.charAt(i);
@@ -53,17 +34,4 @@ public class Spaces {
 		}
 		return white;
 	}
-	private static void output(int count)
-	{
-		try
-		{    
-			FileWriter fw=new FileWriter(path+"/InputOutput/output.txt");
-			String New_Count=String.valueOf(count);
-			fw.write(New_Count);    
-			fw.close();    
-		}catch(Exception e){
-			System.out.println(e);
-		}    
-	}
-
 }
