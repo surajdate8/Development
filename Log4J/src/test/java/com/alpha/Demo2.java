ba70ad1d55b7cd0052e5edb4b597235859ebfad2
suraj <surajdate8@gmail.com>
2020-09-14 13:41:24 +0530

    Initial commit

---
+++ Log4J/src/test/java/com/alpha/Demo2.java
@@ -0,0 +1,13 @@
+package com.alpha;
+
+import org.apache.logging.log4j.*;
+public class Demo2 {
+
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi");		
+	}
+
+}
---
+++ Log4J/src/test/java/com/beta/Demo1.java
@@ -0,0 +1,20 @@
+package com.beta;
+
+import org.apache.logging.log4j.*;
+public class Demo1 {
+
+	private static Logger log=LogManager.getLogger(Demo1.class.getName());
+
+
+	public static void main(String[] args) {
+
+		log.debug("I am debugging");
+		if(2>0) {
+			log.error("Error occured");
+		}		else {
+			log.error("object is not present");
+		}
+		log.fatal("Fatal error");
+		log.info("Button is not displayed");
+	}
+}
