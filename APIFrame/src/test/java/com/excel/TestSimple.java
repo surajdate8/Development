6c1390b8af7c406e26de4480eca87b340e810d47
suraj <surajdate8@gmail.com>
2020-09-14 13:49:23 +0530

    updates

---
+++ APIFrame/src/test/java/com/excel/TestSimple.java
@@ -0,0 +1,24 @@
+package com.excel;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TestSimple {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		
+		DataDriver	 d=new DataDriver();
+	ArrayList data=d.getData("Add Profile");
+	
+//	System.out.println(data.get(0));
+//	System.out.println(data.get(1));
+//	System.out.println(data.get(2));
+//	System.out.println(data.get(3));
+
+	
+		
+	}
+
+}
